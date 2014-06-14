411e57bf0978cc3eaf9f97e0d6a1012dae5f677a
Peter Tuan <xpetahzx@gmail.com>
2014-06-14 16:48:30 -0400

    Fixed ENV

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -37,6 +37,7 @@ group :development, :test do
   gem 'quiet_assets'
   gem 'launchy'
   gem 'pry-rails'
+  gem 'dotenv-rails'
 end
 
 gem 'httparty'
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 	end
 
 	def self.check_for_new
-		all_instagrams = HTTParty.get("https://api.instagram.com/v1/tags/bostonsbad/media/recent?client_id=INSTAGRAM_CLIENT_ID")
+		all_instagrams = HTTParty.get("https://api.instagram.com/v1/tags/bostonsbad/media/recent?client_id=#{ENV["INSTAGRAM_CLIENT_ID"]}")
 		if all_instagrams.code == 200
 			all_instagrams["data"].each do |instagram|
 				post = Post.create(image_url:"#{instagram["images"]["standard_resolution"]["url"]}",caption:"#{instagram["caption"]["text"]}",submitted_by:"#{instagram["user"]["username"]}")
