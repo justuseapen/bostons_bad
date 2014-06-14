84d26a31d4dc11d197e4aba0ff0d7d9f19af3958
Peter Tuan <xpetahzx@gmail.com>
2014-06-14 16:19:55 -0400

    Modified post with new Instagram Client ID hidden

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 	end
 
 	def self.check_for_new
-		all_instagrams = HTTParty.get("https://api.instagram.com/v1/tags/bostonsbad/media/recent?client_id=242df425470e4c3594a7d4111bc7cba5")
+		all_instagrams = HTTParty.get("https://api.instagram.com/v1/tags/bostonsbad/media/recent?client_id=INSTAGRAM_CLIENT_ID")
 		if all_instagrams.code == 200
 			all_instagrams["data"].each do |instagram|
 				post = Post.create(image_url:"#{instagram["images"]["standard_resolution"]["url"]}",caption:"#{instagram["caption"]["text"]}",submitted_by:"#{instagram["user"]["username"]}")
@@ -17,5 +17,5 @@ class Post < ActiveRecord::Base
 			redirect_to root_path
 		end
 		Post.all
-	end	
+  end	
 end
